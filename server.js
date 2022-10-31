8bbb51c062d0e85fe55d7c5cc6b6ad6c4f41bccc
Maxwell Peng <maxp06@email.unc.edu>
2022-10-31 16:37:18 -0400

    1

--- server.js
+++ server.js
@@ -29,6 +29,28 @@
 // 3. end with the data that you are reading in from ./public/index.html.
 
 
+const http = require('http')
+const fs = require('fs')
+const hostname = '127.0.0.1'
+
+const minimist = require('minimist');
+var args = minimist(process.argv);
+const port = args.port || 3000;
+
+const server = http.createServer((req, res) => {
+    var data;
+        try {
+            data = fs.readFileSync('./public/index.html', 'utf8');
+            // console.log(data);
+          } catch (err) {
+            console.error(err);
+          }
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(data)
+})
+
+
 
 
 
